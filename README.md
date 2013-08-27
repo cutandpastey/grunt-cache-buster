9318ff472eed34bbe2359980b4319e524dc40457
Jon Parsons <jon@ctrlxctrlv.co.uk>
2013-08-27 17:22:51 +0100

    change promise resolution in cache_buster task

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 > Bust yo cache like a baws
 
-This grunt task is still very much in development, use it at your own risk :)
-
 ## Getting Started
 This plugin requires Grunt `~0.4.1`
 
--- tasks/cache_buster.js
+++ tasks/cache_buster.js
@@ -29,54 +29,83 @@ module.exports = function (grunt) {
 
         // ASYNC TASK
         var done = this.async();
+
         var targetList = this.data.targets;
+        var promises = [];
 
-        for (var key in targetList) {               //  loop through all assets
-
-            var target = targetList[key];           //  asset from list
-            var asset = target.asset;               //  path to asset file (js/css)
-
-            var assetClone = asset;                 // clone path to asset file (js/css)
-            assetClone = assetClone.split('/')      // break into array
-
-            var assetName = assetClone.pop();       //  asset name
-            var assetPath = assetClone.join('/');   //  asset path
-
-            var templateUrl  = target.target;       //  path to html template
-            var dest    = target.dest;              //  destination html file to save
-            var baseUrl = target.baseUrl;           //  base url to append
-            var regex   = target.regex
-
-            var fileName;
-
-            console.log('Reading asset file', asset);
-            getFileData(asset) // GET DATA AND MD5 HASH OF ALL DATA
-                .then(function (result) {
-                    console.log('Writing asset file', newFilePath);
-                    var fileData = result[0];
-                    var md5Hash = result[1];
-                    fileName = md5Hash + '.' + assetName;
-                    var newFilePath = assetPath + '/' + fileName;
-
-                    return writeNewHashedFile(newFilePath, fileData)
-                })
-                .then(function(){
-                    console.log('Reading template file', templateUrl);
-                    return getFileData(templateUrl)
-                })
-                .then(function(result){
-                    console.log('Writing template file', templateUrl);
-                    var fileData = result[0];
-                    var newUrl = baseUrl + '/' + fileName;
-                    var newFileData = fileData.replace( regex, newUrl);
-
-                    return writeNewHashedFile(dest, newFileData)
-                })
-                .done(done)
-
+        for (var key in targetList) {                       //  loop through all assets
+            promises.push(cacheBustAsset(targetList[key]));
         }
+
+        Q.all(promises).then(function(){
+            done()
+        });
+
     });
 };
+
+var cacheBustAsset = function (target) {
+    var deferred = Q.defer()
+    var fileName;
+
+    var templateUrl = target.target;       //  path to html template
+    var dest = target.dest;              //  destination html file to save
+    var baseUrl = target.baseUrl;           //  base url to append
+    var regex = target.regex
+
+
+    var asset = target.asset;               //  path to asset file (js/css)
+
+    if (asset !== null) {
+        var assetClone = asset;                 // clone path to asset file (js/css)
+        assetClone = assetClone.split('/')      // break into array
+        var assetName = assetClone.pop();       //  asset name
+        var assetPath = assetClone.join('/');   //  asset path
+
+        console.log('Reading asset file', asset);
+        getFileData(asset) // GET DATA AND MD5 HASH OF ALL DATA
+            .then(function (result) {
+                console.log('Writing asset file', newFilePath);
+                var fileData = result[0];
+                var md5Hash = result[1];
+                fileName = md5Hash + '.' + assetName;
+                var newFilePath = assetPath + '/' + fileName;
+
+                return writeNewHashedFile(newFilePath, fileData)
+            })
+            .then(function () {
+                console.log('Reading template file', templateUrl);
+                return getFileData(templateUrl)
+            })
+            .then(function (result) {
+                console.log('Writing template file', templateUrl);
+                var fileData = result[0];
+                var newUrl = baseUrl + '/' + fileName;
+                var newFileData = fileData.replace(regex, newUrl);
+
+                return writeNewHashedFile(dest, newFileData)
+            })
+            .done(function(){
+                deferred.resolve({});
+            })
+    }
+    //todo make this dry
+    //  IF THE ASSET DOES NOT EXIST JUST WRITE THE BASE URL TO THE TEMPLATE
+    else {
+        console.log('Reading asset file', 'no asset');
+        getFileData(templateUrl)
+            .then(function (result) {
+                console.log('Writing template file', templateUrl);
+                var fileData = result[0];
+                var newFileData = fileData.replace(regex, baseUrl);
+                return writeNewHashedFile(dest, newFileData)
+            })
+            .done(function () {
+                deferred.resolve({});
+            })
+    }
+    return deferred.promise;
+}
 
 /*  ------------------------------------------------------------------
  *   READS A FILE AND RETURNS THE FILE DATA AND AN MD5 HAS OF THAT DATA
